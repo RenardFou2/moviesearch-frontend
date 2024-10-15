2c6b2f3a84e14e216ae9dcce6a307465d8a01b6e
Renard <harpi473@Gmail.com>
2024-10-15 19:28:59 +0200

    UI upgrade

--- src/component/MovieList.js
+++ src/component/MovieList.js
@@ -1,34 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './MovieList.css'
 
 const MovieList = () => {
-  const [movies, setMovies] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:8000/api/movies/')
-      .then(response => {
-        setMovies(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching movies:', error);
-      });
+    .then(response => {
+      console.log(response.data);
+      setCategories(response.data || {});
+    })
+    .catch(error => {
+      console.error("Error fetching categories:", error);
+    });
   }, []);
-
+  
   return (
-    <div>
-      <h1>Top Rated Movies</h1>
-      <div className="movie-grid">
-        {movies.map(movie => (
-          <div key={movie.id} className="movie-card">
-            <Link to={`/movies/${movie.id}`}>
-              <img src={movie.poster} alt={movie.title} />
-            </Link>
-            <h3>{movie.title} ({movie.year})</h3>
-            <p>Rating: {movie.rating}</p>
+    <div className="movie-list">
+      {Object.entries(categories).map(([categoryName, movies]) => (
+        <div key={categoryName}>
+          <h2 className="movie-category-title">{categoryName.replace('_', ' ').toUpperCase()}</h2>
+          <div className="movie-row">
+            {movies.map((movie) => (
+              <div key={movie.id} className="movie-card">
+                <Link to={`/movies/${movie.id}`}>
+                  <img src={movie.poster} alt={movie.title} className="movie-poster" />
+                </Link>
+                <h4 className="movie-title">{movie.title}</h4>
+                <p className="movie-info">Rating: {movie.rating}</p>
+                <p className="movie-info">{movie.year}</p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
